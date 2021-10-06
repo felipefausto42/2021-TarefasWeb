2cd17e1e994a5f5c642ed8abc32ce1d283cd715c
Felipe Fausto <fellipe.fausto@hotmail.com>
2021-10-06 15:18:33 -0300

    Instalação de bibliotecas

--- src/components/AppCardBullets.js
+++ src/components/AppCardBullets.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const AppCardBullets = ({date, bullets}) => (
-    <div>
-      <hr />
-      <h5>{date}</h5>
-      <ul>
-        {bullets.map((bullet, index ) => (<li key={index}><strong>{bullet.type}</strong>{bullet.title}</li>))}
-      </ul>
-      <hr />
-      <br />
-    </div>
-    );
+const AppBulletList = (props) => (
+   <div className="card">
+      <div className="card-content">
+         <p className="title">{props.title}</p>
+      </div>
+      <footer className="card-footer">
+         <p className="card-footer-item">
+            <span>
+               View on
+               <a href="https://twitter.com/codinghorror/status/506010907021828096">
+                  Twitter
+               </a>
+            </span>
+         </p>
+         <p className="card-footer-item">
+            <span>
+               Share on <a href="#">Facebook</a>
+            </span>
+         </p>
+      </footer>
+   </div>
+);
 
-export default AppCardBullets; 
+export default AppBulletList;
--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const AppNavBar = props => (
-    <div>
-      <h4>{props.title}</h4>
-    </div>
-    );  
+const AppNavBar = (props) => (
+   <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div id="navbarBasicExample" className="navbar-menu">
+         <div className="navbar-start">
+            <a className="navbar-item">{props.title}</a>
+            <a className="navbar-item">Documentation</a>
+            <div className="navbar-item has-dropdown is-hoverable">
+               <a className="navbar-link">More</a>
+               <div className="navbar-dropdown">
+                  <a className="navbar-item">About</a>
+                  <a className="navbar-item">Jobs</a>
+                  <a className="navbar-item">Contact</a>
+                  <hr className="navbar-divider" />
+                  <a className="navbar-item">Report an issue</a>
+               </div>
+            </div>
+         </div>
+         <div className="navbar-end">
+            <div className="navbar-item">
+               <div className="buttons">
+                  <a className="button is-primary">
+                     <strong>Sign up</strong>
+                  </a>
+                  <a className="button is-light">Log in</a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </nav>
+);
 
-    export default AppNavBar;
+export default AppNavBar;
